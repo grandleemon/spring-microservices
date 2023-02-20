4618aa00a27d12d0293e97dc0feb4f562fda4410
Max <zeetreex@gmail.com>
2023-02-20 19:03:31 +0200

    maven init with archetype:generate

---
+++ microservices/src/main/java/com/springexp/App.java
@@ -0,0 +1,13 @@
+package com.springexp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
